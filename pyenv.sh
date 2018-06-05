b866d5a4d9cecf3d0b46700a679ff37fb809e5e4
yoshiken <yskn.beer@gmail.com>
2018-06-05 13:32:04 +0900

    add pyenv

---
+++ pyenv.sh
@@ -0,0 +1,11 @@
+#!/bun/sh
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+if [ `which fish` ]; then
+    echo 'set PYENV_ROOT "$HOME/.pyenv"' >> ~/.config/fish/config.fish
+    echo 'set PATH "$PYENV_ROOT/bin $PATH"' >> ~/.config/fish/config.fish
+    echo '. (pyenv init - | psub)' >> ~/.config/fish/config.fish
+else
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+    echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+fi
